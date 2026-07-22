45c0bc3af1088291de02f0dd11273753795f205e
DraconicLogic <39361516+DraconicLogic@users.noreply.github.com>
2026-07-22 23:00:57 +0000

    Setup Starter homepage

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Chic Chi Store</h1>
+            <p>Find the perfect Chic fashion for your lifestyle.</p>
+        </div>
+    );
+}
